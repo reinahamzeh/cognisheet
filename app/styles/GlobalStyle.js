4a9608f2851c68940006f4ab5383bb3135fc4c0e
reinahamzeh <reina.hamzeh26@gmail.com>
2025-03-22 17:26:57 +0100

    moved to app

---
+++ app/styles/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+`;
+
+export default GlobalStyle; 
